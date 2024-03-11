b3c2a12fb538e23fd11f00d1b0d31337f45a072f
happybirthdaynibbi <129252118+happybirthdaynibbi@users.noreply.github.com>
2024-03-11 21:05:43 +0530

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -97,25 +97,25 @@ const Welcome = styled.div`
 `
 
 export function WelcomeBanner() {
-  return (
-    <Welcome>
-      <div>
-        <h1>Welcome to Gamba v2 👋</h1>
-        <p>
-          A fair, simple and decentralized casino on Solana.
-        </p>
-      </div>
-      <Buttons>
-        <button onClick={() => window.open('https://v2.gamba.so/', '_blank')}>
-          🚀 Add Liqudity
-        </button>
-        <button onClick={() => window.open('https://github.com/gamba-labs/gamba', '_blank')}>
-          👨‍💻 Build your own
-        </button>
-        <button onClick={() => window.open('https://discord.gg/HSTtFFwR', '_blank')}>
-          💬 Discord
-        </button>
-      </Buttons>
-    </Welcome>
+  return (<></>
+    // <Welcome>
+    //   <div>
+    //     <h1>Welcome to Gamba v2 👋</h1>
+    //     <p>
+    //       A fair, simple and decentralized casino on Solana.
+    //     </p>
+    //   </div>
+    //   <Buttons>
+    //     <button onClick={() => window.open('https://v2.gamba.so/', '_blank')}>
+    //       🚀 Add Liqudity
+    //     </button>
+    //     <button onClick={() => window.open('https://github.com/gamba-labs/gamba', '_blank')}>
+    //       👨‍💻 Build your own
+    //     </button>
+    //     <button onClick={() => window.open('https://discord.gg/HSTtFFwR', '_blank')}>
+    //       💬 Discord
+    //     </button>
+    //   </Buttons>
+    // </Welcome>
   )
 }
